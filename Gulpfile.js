1a8fe79c81fd858f91f6eb13d4e6329070851924
charles peters <charlespeters42@gmail.com>
2016-02-21 00:47:38 -0800

    listing tasks and comments

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,10 @@ const postcss    = require('gulp-postcss');
 const size       = require('gulp-size');
 const rename     = require('gulp-rename');
 const imagemin   = require('gulp-imagemin');
+const jscs       = require('gulp-jscs');
+const reporter   = require('postcss-reporter');
+const bemLinter  = require('postcss-bem-linter');
+const stylelint  = require('stylelint');
 const cssnext    = require('postcss-cssnext');
 const atImport   = require('postcss-import');
 const browserify = require('browserify');
@@ -12,22 +16,64 @@ const source     = require('vinyl-source-stream');
 const buffer     = require('vinyl-buffer');
 const del        = require('del');
 const bs         = require('browser-sync').create();
-
 const metalsmith = require('./Metalsmith');
 const paths      = require('./paths');
+const lintconfig = require('./stylelint.config');
 
+// Configuration
 const processors = [
   cssnext,
   atImport
 ];
 
-gulp.task('styles', ()=> {
+const cssLinters = [
+  stylelint({ rules: lintconfig.rules }),
+  bemLinter('bem'),
+  reporter({ clearMessages: true }),
+];
+
+const sizeConfig = {
+  gzip: true,
+  pretty: true,
+  showFiles: true
+};
+
+// Content Tasks
+/////////////////////
+
+// Build Site through Metalsmith with presets
+gulp.task('metalsmith', ['clean'], metalsmith());
+gulp.task('metalsmith:prod', ['clean'], metalsmith(true));
+
+// Linting Tasks
+/////////////////////
+
+// Lint JavaScript with JSCS
+gulp.task('lint:js', ()=> {
+  return gulp.src(paths.js.src)
+    .pipe(jscs());
+});
+
+// Lint CSS with Stylelint
+gulp.task('lint:css', ()=> {
+  return gulp.src([paths.css.src, paths.css.all])
+    .pipe(postcss(cssLinters));
+});
+
+// Asset Tasks
+/////////////////////
+
+// CSS Compilation with PostCSS
+gulp.task('styles', ['lint:css'], ()=> {
   return gulp.src(paths.css.src)
     .pipe(postcss(processors))
     .pipe(rename('main.css'))
-    .pipe(gulp.dest(paths.css.dest));
+    .pipe(size(sizeConfig))
+    .pipe(gulp.dest(paths.css.dest))
+    .pipe(bs.stream());
 });
 
+// Transpile JavaScript through Browserify with Babel
 const bundler = watchify(browserify(paths.js.src, watchify.args));
 
 function bundle() {
@@ -37,21 +83,39 @@ function bundle() {
     .pipe(source('./bundle.js'))
       .pipe(buffer())
     .pipe(gulp.dest(paths.js.dest))
-    .pipe(size({ showFiles: true }));
+    .pipe(size(sizeConfig))
+    .pipe(bs.stream());
 };
 
-gulp.task('scripts', bundle);
 bundler.on('update', bundle);
+gulp.task('scripts', ['lint:js'], bundle);
 
-gulp.task('images', ()=> { });
+// Optimize Images
+gulp.task('images', ()=> {
+  return gulp.src(paths.img.src)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+    }))
+    .pipe(size(sizeConfig))
+    .pipe(gulp.dest(paths.img.dest))
+    .pipe(bs.stream());
+});
 
-gulp.task('metalsmith', metalsmith());
-gulp.task('metalsmith:prod', metalsmith(true));
+// Utility Tasks
+/////////////////////
 
+// rm -rf the Build directory
+gulp.task('clean', () => {
+  return del(paths.build);
+});
+
+// Watch files for changes
 gulp.task('watch', ()=> {
 
 });
 
+// Starts Browser Sync Server
 gulp.task('connect', () => {
   bs.init({
     server: {
@@ -59,6 +123,9 @@ gulp.task('connect', () => {
     }
   });
 });
+
+// Default Tasks
+/////////////////////
 
 gulp.task('assets', ['styles', 'images', 'scripts', 'icons']);
 gulp.task('build', ['metalsmith', 'assets']);
